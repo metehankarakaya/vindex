fc57db4bda2e10a3cd698bab006953466b5e6618
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-04-28 18:50:48 +0300

    feat: setup go_router with bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vindex/router/app_router.dart';
 
 void main() {
   runApp(
@@ -14,13 +15,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("Vindex"),
-        ),
-      ),
+      routerConfig: appRouter,
     );
   }
 }
