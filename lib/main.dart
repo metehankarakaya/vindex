411ffe9ec9ed5a06860fb6fab3f0a5d0944afe96
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-04-29 01:15:06 +0300

    feat: setup easy_localization with tr and en support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vindex/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   runApp(
-    const ProviderScope(
-      child: MyApp()
+    EasyLocalization(
+      supportedLocales: const [
+        Locale("en", "US"),
+        Locale("tr"),
+      ],
+      saveLocale: true,
+      path: "assets/translations",
+      fallbackLocale: Locale("en", "US"),
+      child: const ProviderScope(
+        child: MyApp()
+      ),
     )
   );
 }
@@ -17,6 +30,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       routerConfig: appRouter,
       theme: ThemeData(colorSchemeSeed: const Color(0xFF2563EB)),
     );
